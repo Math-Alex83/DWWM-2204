a55cc379adb3ed14632ec412e3189eb8077eb487
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2022-05-12 10:47:39 +0200

    modif

--- 01-Algo/3_Partie3/3.4_Recherche_des_diviseur_d'un_nombre.js
+++ 01-Algo/3_Partie3/3.4_Recherche_des_diviseur_d'un_nombre.js
@@ -1,22 +1,21 @@
 /*# VARIABLES
 
 ENTIER nb1 <-- A
-ENTIER div <-- A
+ENTIER div <-- A - 1
 
 # DEBUT DU PROGRAMME
 
 LIRE nb1
 LIRE div
-
-TANT QUE div !=1
+ECRIRE "Les diviseur de " + nb1 + " sont"
+TANT QUE div >1
     LIRE nb1
     LIRE div
-    SI nb1 % div =0
-        ECRIRE div , " "
+    SI nb1 % div = 0
+        ECRIRE div
     FIN SI
     div <-- div - 1
-FIN TANT QUE
-
+FIN TANT QUE 
 # FIN DU PROGRAMME
 ___________________________________________________________________________
 ___________________________________________________________________________
@@ -33,5 +32,20 @@ while(div !=1)
         resultat += " , " + div
     }       
     div -= 1
+}
+alert("Le(s) diviseur(s) de " + nb1 + " : \n" + resultat)
+
+//nb1=Math.floor((Math.random() * 100) + 1)
+nb1=prompt("Entrez un nombre entier superieur a 1 : ")
+div=nb1-1
+resultat="" + (div+1)
+
+while(div >1)
+{    
+    if(nb1 % div == 0)
+    {
+        resultat += " , " + div
+    }       
+    div--
 }
 alert("Le(s) diviseur(s) de " + nb1 + " : \n" + resultat)
